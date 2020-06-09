0f92157b3efa58f5dbb40be04a6f46f1408752b8
RajapandiR <rajapandibsc12@gmail.com>
2020-06-09 11:02:10 +0530

    added Apiview

--- productapp/api.py
+++ productapp/api.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView, Response
 
 from productapp import serializers
 from productapp import models
@@ -14,3 +15,24 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
 	serializer_class = serializers.ProductSerializer
 	queryset = models.Product.objects.all()
+
+# class ProductApiView(APIView):
+# 	serializer_class = serializers.ProductSerializer
+# 	def get(self, request,  format = None):
+# 		obj = models.Product.objects.all()
+# 		serializer = serializers.ProductSerializer(obj, many=True)
+# 		return Response(serializer.data)
+class CategoryApiView(APIView):
+	serializer_class = serializers.CategorySerializer
+	def get(self, request,  format = None):
+		obj = models.Category.objects.all()
+		serializer = serializers.CategorySerializer(obj, many=True)
+		return Response(serializer.data)
+
+
+class ProductApiView(APIView):
+	serializer_class = serializers.ProductSerializer
+	def get(self, request,  format = None):
+		obj = models.Product.objects.all()
+		serializer = serializers.ProductSerializer(obj, many=True)
+		return Response(serializer.data)
--- productapp/urls.py
+++ productapp/urls.py
@@ -6,8 +6,11 @@ from productapp import api
 router = routers.DefaultRouter()
 router.register('profile', api.ProfileViewSet)
 router.register('category', api.CategoryViewSet)
-router.register('product', api.ProductViewSet)
+# router.register('product', api.ProductViewSet)
+# router.register('product', api.ProductApiView.as_view())
 
 urlpatterns = [
-	path('api/', include(router.urls))
+	path('api/', include(router.urls)),
+	path('product/', api.ProductApiView.as_view()),
+	path('category/', api.CategoryApiView.as_view()),
 ]
